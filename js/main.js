902d09ef3f642bb660cd8277c4eba5bc75d2c012
Albert <albert_zteo@mail.ru>
2022-01-30 21:10:41 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import { intervalToDuration  } from 'date-fns'
+
+const UI = {
+    FORM: document.querySelector("form"),
+    DATE: document.querySelector('input'),
+    RESULT: document.getElementById('result'),
+}
+
+UI.FORM.addEventListener("change", function (e) {
+    const todayDate = new Date();
+    const selectDate = new Date(UI.DATE.value);
+    const difference =  intervalToDuration({ start: todayDate, end: selectDate});
+    UI.RESULT.textContent = `Up to date ${difference.years} year(s), ${difference.days} day(s), ${difference.hours} hour(s)`
+})
